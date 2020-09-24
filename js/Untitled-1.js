a3292c4f90371d4bb779112b0f3ee13b224344de
Joseph Mead <josephmmead@gmail.com>
2020-09-23 20:11:55 -0400

    HTML content created and getting basis for js layed out

---
+++ js/Untitled-1.js
@@ -0,0 +1,9 @@
+// set up vars
+
+// need to set up Week day, month, and time to be set in the header
+
+// set text inputed within <textareas> to be saved to local storage via the save button
+
+// set up save buttons on click function
+
+// change color of time rows based on the time it is
