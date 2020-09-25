6c180f97bc7b66a3230a462f0bcb16ea2f86199f
Joseph Mead <josephmmead@gmail.com>
2020-09-24 20:08:47 -0400

    added updating date

--- js/script.js
+++ js/script.js
@@ -1,5 +1,17 @@
 // set up vars
 
+
+$(document).ready(function() {
+    checkTheDay();
+});
+    
+function checkTheDay(){
+    setInterval(function(){
+    document.getElementById("currentDay").innerHTML = moment().format('MMMM Do YYYY, h:mm:ss a'); 
+  }, 1000);
+}
+
+
 // need to set up Week day, month, and time to be set in the header
 
 // set text inputed within <textareas> to be saved to local storage via the save button
